d58e65e45dccffb3eeb52fe952591987c7c521e4
quintessentialdevrev <63969941+quintessentialdevrev@users.noreply.github.com>
2023-07-25 19:42:22 +0530

    working + responsive

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,5 +2,5 @@ import { NextResponse } from "next/server"
 
 export async function POST(req: Request, res: Response) {
   const body = await req.json()
-  return NextResponse.json({xAxis: [body.user], yAxis: [body.hashtag]})
+  return NextResponse.json({sentiment: "Positive"})
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,63 @@ import { useState } from "react"
 
 export default function Home() {
   const [data, setData] = useState({
+    tweet:'',
     user:'',
     hashtag:''
   })
 
+  const [info, setInfo] = useState({
+    tweet: true,
+    user:false,
+    hashtag:false
+  })
+
     
   const handleSubmit = async () => {
-    const JSONdata = JSON.stringify(data)
-    const endpoint = '/api'
+    const JSONdata = JSON.stringify(data.tweet)
+    const endpoint = 'https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-sentiment'
     const options = {
-      method: 'post',
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'Authorization': "Bearer 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"
       },
-      body: JSONdata,
+      body: JSONdata,    
     }
     const response = await fetch(endpoint, options)
     const result = await response.json()
-    alert(`Is this your full name: ${result.data}`)
+    if(result[0][0].label == "LABEL_0"){
+      alert("Sentiment is Negative")
+    } else if (result[0][0].label == "LABEL_1") {
+      alert("Sentiment is Neutral")
+    } else {
+      alert("Sentiment is Positive")
+    }
   }
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSubmitNew = async () => {
+    const JSONdata = JSON.stringify(data.user)
+    const endpoint = 'https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-sentiment'
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer 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"
+      },
+      body: JSONdata,    
+    }
+    const response = await fetch(endpoint, options)
+    const result = await response.json()
+    if(result[0][0].label == "LABEL_0"){
+      alert("Sentiment is Negative")
+    } else if (result[0][0].label == "LABEL_1") {
+      alert("Sentiment is Neutral")
+    } else {
+      alert("Sentiment is Positive")
+    }
+  }
+
+  const handleChange = (e: any) => {
     setData((prev)=>({...prev, [e.target.name]: e.target.value}))
   }
 
@@ -45,26 +81,26 @@ export default function Home() {
         <h1 className="text-5xl mx-auto text-center leading-snug font-semibold max-w-[47rem]">Generate Sentimental Analysis for anything Twitter in seconds...</h1>
         <p className="text-[#5C6169] text-center py-4">500 tweets analysed so far</p>
         <div className="flex justify-center items-center rounded-lg border-[1px] my-2 border-solid border-[#E5E7EB] w-72 h-9 mx-auto">
-            <button className="p-1 m-1 rounded-lg focus:bg-[#16181D] w-20 h-7 text-xs focus:text-white text-black flex justify-center items-center" autoFocus>
+            <button style={{backgroundColor: info.tweet ? '#16181D' : '', color: info.tweet ? '#FFFFFF' : ''}} className="p-1 m-1 rounded-lg w-20 h-7 text-xs text-black flex justify-center items-center" autoFocus onClick={()=>{setInfo(prev=> {return{ tweet: true, user: false, hashtag: false}})}}>
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M18.0706 8.16384C17.6743 8.33438 17.2577 8.45314 16.8311 8.51718C17.0305 8.48298 17.324 8.12395 17.4408 7.97863C17.6183 7.75942 17.7536 7.50918 17.8398 7.24061C17.8398 7.22067 17.8597 7.19217 17.8398 7.17792C17.8297 7.17244 17.8185 7.16956 17.807 7.16956C17.7956 7.16956 17.7843 7.17244 17.7742 7.17792C17.311 7.4288 16.8179 7.62027 16.3068 7.74782C16.2889 7.75326 16.27 7.75375 16.2519 7.74923C16.2338 7.74471 16.2173 7.73536 16.2042 7.72217C16.1644 7.67479 16.1216 7.63006 16.076 7.58825C15.8675 7.40145 15.6309 7.24856 15.375 7.13518C15.0295 6.99344 14.6563 6.93206 14.2836 6.95567C13.922 6.9785 13.569 7.0755 13.2464 7.24061C12.9288 7.41469 12.6497 7.65119 12.4258 7.93588C12.1903 8.22891 12.0202 8.56897 11.9271 8.9332C11.8503 9.27965 11.8416 9.63773 11.9015 9.9875C11.9015 10.0473 11.9015 10.0559 11.8502 10.0473C9.81851 9.74815 8.15157 9.02723 6.78953 7.47997C6.72969 7.41158 6.69834 7.41158 6.6499 7.47997C6.05721 8.3804 6.34501 9.80514 7.08587 10.509C7.1856 10.603 7.28818 10.6942 7.39646 10.7797C7.05678 10.7555 6.7254 10.6635 6.42194 10.509C6.36496 10.4719 6.33361 10.4919 6.33076 10.5602C6.32268 10.6551 6.32268 10.7504 6.33076 10.8452C6.39022 11.2996 6.56928 11.73 6.84965 12.0925C7.13002 12.4549 7.50162 12.7365 7.92647 12.9082C8.03003 12.9526 8.13795 12.986 8.24846 13.0079C7.93399 13.0699 7.61148 13.0795 7.29388 13.0364C7.2255 13.0222 7.19985 13.0592 7.2255 13.1248C7.64437 14.2646 8.55335 14.6122 9.22013 14.806C9.31131 14.8202 9.40249 14.8202 9.50507 14.843C9.50507 14.843 9.50507 14.843 9.48798 14.8601C9.29136 15.2191 8.49636 15.4613 8.13163 15.5867C7.4659 15.8258 6.75611 15.9172 6.05151 15.8546C5.94038 15.8375 5.91474 15.8403 5.88624 15.8546C5.85775 15.8688 5.88624 15.9002 5.91759 15.9287C6.06006 16.0227 6.20254 16.1053 6.35071 16.1851C6.79182 16.4257 7.25816 16.6168 7.74125 16.755C10.2431 17.4446 13.0584 16.9374 14.9362 15.071C16.4122 13.6063 16.9308 11.5861 16.9308 9.56293C16.9308 9.486 17.0248 9.4404 17.079 9.40051C17.4524 9.10955 17.7816 8.76593 18.0563 8.3804C18.1039 8.32293 18.1283 8.24976 18.1247 8.17524V8.17524C18.1247 8.1325 18.1247 8.14105 18.0706 8.16384Z" fill="#D9D9D9"/>
                 </svg>
                 <p className="mx-1">Tweet</p>
             </button>
-            <button className="p-1 m-1 rounded-lg focus:bg-[#16181D] w-20 h-7 text-xs focus:text-white text-black flex justify-center items-center">@ Handle</button>
-            <button className="p-1 m-1 rounded-lg focus:bg-[#16181D] w-20 h-7 text-xs focus:text-white text-black flex justify-center items-center"># Hashtag</button>
+            <button style={{backgroundColor: info.user ? '#16181D' : '', color: info.user ? '#FFFFFF' : ''}} className="p-1 m-1 rounded-lg w-20 h-7 text-xs text-black flex justify-center items-center" onClick={()=>{setInfo(prev=> {return{ tweet: false, user: true, hashtag: false}})}}>@ Handle</button>
+            <button style={{backgroundColor: info.hashtag ? '#16181D' : '', color: info.hashtag ? '#FFFFFF' : ''}} className="p-1 m-1 rounded-lg w-20 h-7 text-xs text-black flex justify-center items-center" onClick={()=>{setInfo(prev=> {return{ tweet: false, user: false, hashtag: true}})}}># Hashtag</button>
         </div>
-        <div className="mx-auto max-w-2xl m-4">
-            <section className="flex m-2">
-                <div className="w-7 h-7 rounded-full text-white bg-black mx-2 flex justify-center items-center">1</div>
+        <div className="mx-auto max-w-2xl m-4 p-2">
+            <section className="flex mr-2 my-4">
+                <div className="w-7 h-7 rounded-full text-white bg-black mr-2 flex justify-center items-center">1</div>
                 <p className="font-medium justify-center items-center"><span>Copy the tweet </span><span className="text-[#5C6169]">(or write a few words )</span></p>
-            </section>
-            <textarea name="tweet" className="border-2 m-2 p-2 border-solid border-[#E5E7EB] w-[39rem] h-24 block" placeholder="e.g. Yes, we know. Everyone likes coffee. You’re probably not even a “real writer” if you don’t have coffee siphoned down your throat as a form of alarm clock."/>
-            <button className="w-[39rem] h-11 m-2 bg-black text-white rounded-lg">Generate Analysis</button>
+            </section>  
+            { info.tweet ? ( <textarea name="tweet" value={data.tweet} onChange={handleChange} className="border-2 mx-auto m-2 p-2 border-solid border-[#E5E7EB] w-[95%] h-24 block" placeholder="e.g. Yes, we know. Everyone likes coffee. You’re probably not even a “real writer” if you don’t have coffee siphoned down your throat as a form of alarm clock." />) : <></>}
+            { info.user ? ( <input type="text" name="user" value={data.user} onChange={handleChange} className="border-2 m-2 p-2 border-solid border-[#E5E7EB] w-64 h-12 mx-auto block" placeholder="e.g. @SatvamThakkar" />) : <></> } 
+            { info.hashtag ? ( <input type="text" name="hashtag" value={data.hashtag} onChange={handleChange} className="border-2 m-2 p-2 border-solid border-[#E5E7EB] w-64 h-12 mx-auto block" placeholder="e.g. #Coffee" />) : <></> }
+            { info.tweet ? (<button className="w-[95%] h-11 m-2 bg-black text-white rounded-lg" onClick={handleSubmit}>Generate Analysis</button>) : 
+            (<button className="w-[39rem] h-11 m-2 bg-black text-white rounded-lg" onClick={handleSubmitNew}>Generate Analysis</button>) }
         </div>
-        <input type="text" name="user" value={data.user} onChange={handleChange} />
-        <input type="text" name="hashtag" value={data.hashtag} onChange={handleChange} />
-        <button className="" type="submit" onSubmit={handleSubmit}></button>
     </>
   )
 }
